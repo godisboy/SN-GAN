442246fb121960d3fd8bd64eccd9bb61f9196d0c
jiaming <memono.liu@gmail.com>
2018-04-04 15:14:42 -0700

    change {snconv2d|snlinear}.u to buffer.

--- src/snlayers/snlinear.py
+++ src/snlayers/snlinear.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.modules import Linear
-from src.functions.max_sv import max_singular_value
+from ..functions.max_sv import max_singular_value
 
 class SNLinear(Linear):
     r"""Applies a linear transformation to the incoming data: :math:`y = Ax + b`
@@ -27,13 +27,13 @@ class SNLinear(Linear):
        """
     def __init__(self, in_features, out_features, bias=True):
         super(SNLinear, self).__init__(in_features, out_features, bias)
-        self.u = nn.Parameter(torch.Tensor(1, out_features).normal_())
+        self.register_buffer('u', torch.Tensor(1, out_features).normal_())
 
     @property
     def W_(self):
         w_mat = self.weight.view(self.weight.size(0), -1)
         sigma, _u = max_singular_value(w_mat, self.u)
-        self.u.data = _u
+        self.u.copy_(_u)
         return self.weight / sigma
 
     def forward(self, input):
